8a90a1fef4a7a2dea4be7acced254f9f58f2f8d3
PastoreNur <julio.pastore.jp@gmail.com>
2016-11-05 01:29:45 -0600

    Inicio
    Este es el incio del fin

---
+++ App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Sistema_CECEES.Startup))]
+namespace Sistema_CECEES
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
