8253cba6f202fd4a3c0aaab415782457e6a09cdb
c3rberuss <aaamaya98@gmail.com>
2016-11-12 02:56:41 +0100

    login funcionando con access

--- Default.aspx
+++ Default.aspx
@@ -4,6 +4,6 @@
    
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
-    <h1>Puto :v</h1>
+    <h1>¡Bienvenido!</h1>
 </asp:Content>
 
--- Login.aspx.cs
+++ Login.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.OleDb;
 
 public partial class Login : System.Web.UI.Page
 {
@@ -18,23 +19,29 @@ public partial class Login : System.Web.UI.Page
 
     protected void Button1_Click(object sender, EventArgs e)
     {
+        OleDbConnection conexion = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\josue\OneDrive\Documents\GitHub\Sistema-de-CECEES\App_Data\sistema_cecees.accdb;Persist Security Info=True");
+
+        conexion.Open();
+
+        string consulta = "SELECT user_name, password from usuarios WHERE user_name='"+TextBox1.Text+"'AND password='"+TextBox2.Text+"';";
+        OleDbCommand Ejecutar_Consulta = new OleDbCommand(consulta,conexion);
+
+        OleDbDataReader LectorDatos = Ejecutar_Consulta.ExecuteReader();
+
+        Boolean Validar_Usuario = LectorDatos.HasRows;
+
         try
         {
-            if(TextBox1.Text == "admin" || TextBox1.Text == "Admin")
+            if (Validar_Usuario)
             {
-                if(TextBox2.Text == "1234")
-                {
-                    this.Page.Response.Write("<script language='JavaScript'>window.alert('Ha iniciado Sesión');</script>");
-                    System.Diagnostics.Process.Start("http://www.xvideos.com");
-                }
-                else
-                {
-                    this.Page.Response.Write("<script language='JavaScript'>window.alert('Contraseña Incorrecta');</script>");
-                }
+                this.Page.Response.Write("<script language='JavaScript'>window.alert('Ha iniciado Sesión');</script>");
+                System.Diagnostics.Process.Start("http://www.Google.com");
+                conexion.Close();
+                
             }
             else
             {
-                this.Page.Response.Write("<script language='JavaScript'>window.alert('Usuario Incorrecto');</script>");
+                this.Page.Response.Write("<script language='JavaScript'>window.alert('Usuario o Contaseña incorrecta');</script>");
             }
         }
         catch
